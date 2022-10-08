0518fd9b83eebae9e7618381f7d944fdad4f8434
Ribosome2 <583154233@qq.com>
2022-10-08 08:24:38 +0800

    Camera Transformation(WIP)

--- rasengan/src/EngineCore/Camera.cpp
+++ rasengan/src/EngineCore/Camera.cpp
@@ -4,29 +4,32 @@
 #include "glm/ext/matrix_clip_space.hpp"
 
 void Camera::OnGUI() {
-    ImGui::SliderFloat3("eyePos ", (float *) &eyePos, -10, 10);
-    static auto lookAtCenter = glm::vec3(0.0f, 0.0f, 0.0f);
-    ImGui::SliderFloat3("lookAtCenter ", (float *) &lookAtCenter, -10, 10);
-    ImGui::SliderFloat("pitch ", (float *) &pitch, -89, 90);
-    ImGui::SliderFloat("yaw ", (float *) &yaw, -89, 89);
+	ImGui::SliderFloat3("eyePos ", (float *) &eyePos, -10, 10);
+	static auto lookAtCenter = glm::vec3(0.0f, 0.0f, 0.0f);
+	ImGui::SliderFloat3("lookAtCenter ", (float *) &lookAtCenter, -10, 10);
+	ImGui::SliderFloat("pitch ", (float *) &pitch, -89, 90);
+	ImGui::SliderFloat("yaw ", (float *) &yaw, -89, 89);
 }
 
 void Camera::UpdateDataMatrix() {
 
-    UpdateFrontDirection();
-    worldToCameraMatrix = glm::lookAt(eyePos, eyePos + cameraFront, cameraUp);
-    projectionMatrix = glm::perspective(glm::radians(45.0f), aspect, nearClipPlane, farClipPlane);
-    projectionMatrix[1][1] *= -1;
+	UpdateFrontDirection();
+	worldToCameraMatrix = glm::lookAt(eyePos, eyePos + cameraFront, cameraUp);
+	projectionMatrix = glm::perspective(glm::radians(45.0f), aspect, nearClipPlane, farClipPlane);
+	projectionMatrix[1][1] *= -1;
 }
 
 void Camera::UpdateFrontDirection() {
-    glm::vec3 direction;
-    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    direction.y = sin(glm::radians(pitch));
-    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-    cameraFront = glm::normalize(direction);
+	cameraFront = glm::vec3{
+			cos(glm::radians(yaw)) * cos(glm::radians(pitch)),
+			sin(glm::radians(pitch)),
+			sin(glm::radians(yaw)) * cos(glm::radians(pitch))
+	};
+
+	cameraFront = glm::normalize(cameraFront);
+	//todo: 算出up 和right
 }
 
 Camera::Camera() {
-    UpdateFrontDirection();
+	UpdateFrontDirection();
 }
