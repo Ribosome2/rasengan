a11c2bb6e3a290c29a3fa14ccdb5c2c4c412dd12
Ribosome2 <583154233@qq.com>
2022-10-09 10:09:24 +0800

    now that we can see the whole cube , but it does not look like a perspective projection

--- rasengan/src/EngineCore/GameObject.cpp
+++ rasengan/src/EngineCore/GameObject.cpp
@@ -1,8 +1,11 @@
 #include "GameObject.h"
 #include "MeshRenderer.h"
+#include "Time.h"
+
 uint32_t  GameObject::s_autoInstanceId =0;
 void GameObject::Update() {
   meshRenderer->Update();
+  transform.eulerAngles.y+= Time::deltaTime*50;
 }
 
 GameObject::GameObject() {
